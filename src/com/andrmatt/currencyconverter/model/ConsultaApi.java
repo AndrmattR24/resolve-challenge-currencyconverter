6a0d13bbb2bfbc370e7cf475830ae654e847b202
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-04-21 21:18:50 -0500

    Creando clase que se encargara de hacer la consulta a la api y obtener el tipo de cambio

--- src/com/andrmatt/currencyconverter/model/ConsultaApi.java
+++ src/com/andrmatt/currencyconverter/model/ConsultaApi.java
@@ -1,2 +1,35 @@
-package com.andrmatt.currencyconverter.model;public class ConsultaApi {
+package com.andrmatt.currencyconverter.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaApi {
+    final String API_KEY = "94d094cd60be36dda8296269";
+
+    public ExchangeRates obtenerCambio (String currency_1, String currency_2 ,double amount){
+
+        final URI URL_QUERY = URI
+                .create("https://v6.exchangerate-api.com/v6/"+API_KEY+"/pair/"+currency_1+"/"+currency_2+"/"+amount);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URL_QUERY)
+                .build();
+
+        try {
+         HttpResponse<String> response = null;
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(), ExchangeRates.class);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
